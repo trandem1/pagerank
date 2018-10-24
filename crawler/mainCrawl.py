df73ed9deae067afca8ebe07a30968bd92622445
trandem1 <demtran1234@gmail.com>
2018-10-24 20:15:45 +0700

    in ket qua ra file

--- crawler/mainCrawl.py
+++ crawler/mainCrawl.py
@@ -11,11 +11,22 @@ def getAllDestLink (link):
             if(str(link.get('href')).startswith('/wiki')):
                 destLink = prefix + link.get('href')
                 destLinks.append(destLink)
+        req.close()
     except:
         pass
     # destLinks.remove("")
     return  destLinks
 
 
+def getTitleLink (link):
+    try:
+        req = requests.get(link)
+        soup = BeautifulSoup(req.text,"lxml")
+        title = soup.find('title').string
+        req.close()
+        return  str(title)
+    except:
+        return "khong tim thay title"
 
+# print(getTitleLink('https://vi.wikipedia.org/wiki/Tổng_Bí_thư_Ban_Chấp_hành_Trung_ương_Đảng_Cộng_sản_Việt_Nam'))
 # x =getAllDestLink('https://vi.wikipedia.org/wiki/Tổng_Bí_thư_Ban_Chấp_hành_Trung_ương_Đảng_Cộng_sản_Việt_Nam')
--- main/main.py
+++ main/main.py
@@ -4,27 +4,47 @@ import handleGraph.createGraph as graph
 import handleGraph.pageRank as pageRank
 import operator
 
+def writeToFile(fileName,startPath,numberNode,pageRanks,maxPrint = 100):
+    printedCheck = 0
+    f = open(fileName,"w")
+    startTitle = crawl.getTitleLink(startPath)
+    f.write(startTitle +"\t" + str(numberNode))
+    f.write("\n")
+    f.write("Pagerank"+"\t"+"Title")
+    f.write("\n")
+    for k,v in pageRanks:
+        printedCheck = printedCheck +1
+        # print(str(k) +"\t" +str(v))
+        f.write(str("{:1.4f}".format(v)) +"\t" + crawl.getTitleLink(str(k)))
+        f.write("\n")
+        if printedCheck > maxPrint:
+            break
+    f.close()
+
+
 if __name__ == '__main__':
-    maxNode = 10000 #max so url
+    ### khoi tao tham so
+    maxNode = 5000 #max so url
     startPath = 'https://vi.wikipedia.org/wiki/Tổng_Bí_thư_Ban_Chấp_hành_Trung_ương_Đảng_Cộng_sản_Việt_Nam'
+
     destPaths = crawl.getAllDestLink(startPath)
     G = graph.buildDiGraph()
     graph.addEdgeToGraph(G,startPath,destPaths)
     ### add more node in the graph
     for souceUrl in destPaths :
         destUrl = crawl.getAllDestLink(souceUrl)
+        print("size of graph =" + str(G.__len__()))
         if G.__len__() < maxNode:
             graph.addEdgeToGraph(G,souceUrl,destUrl)
         else:
             break
+
     pr = pageRank.evaluatePageRank(G,0.85)
+
     sortedPr = sorted(pr.items(),key=operator.itemgetter(1),reverse=True)
-    # print(sortedPr)
-    maxPrint = 100
-    printedCheck = 0
-    for k,v in sortedPr:
-        printedCheck = printedCheck +1
-        print(str(k) +"\t" +str(v))
-        if printedCheck > maxPrint:
-            break
+    print("start write")
+    writeToFile("20181_IT4868_Assignment01_GroupXXX_ranking.txt",startPath,G.__len__(),sortedPr,100)
+
+
+
 
